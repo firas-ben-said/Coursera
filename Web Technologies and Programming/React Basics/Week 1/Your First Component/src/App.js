e9b58b88f9df111d880b146571de7b79134361b3
Firas Ben Said <firas.ben.said@horizon-tech.tn>
2024-02-19 15:44:51 +0100

    Lab: Creating and importing components

--- Web Technologies and Programming/React Basics/Week 1/Your First Component/src/App.js
+++ Web Technologies and Programming/React Basics/Week 1/Your First Component/src/App.js
@@ -1,13 +1,8 @@
-function Heading() {
-  return (
-    <h1>This is an h1 heading</h1>
-  );
-}
+import Heading from "./Heading";
 
 function App() { 
   return ( 
     <div className="App"> 
-      This is the starting code for "Your first component" ungraded lab 
       <Heading /> 
     </div> 
   ); 
