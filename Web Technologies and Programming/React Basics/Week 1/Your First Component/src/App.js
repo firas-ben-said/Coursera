0204810ba766717af7707ae7ffcfa8c0254e5ad0
Firas Ben Said <firas.ben.said@horizon-tech.tn>
2024-02-20 01:12:39 +0100

    Lab : Props and multiple components

--- Web Technologies and Programming/React Basics/Week 1/Your First Component/src/App.js
+++ Web Technologies and Programming/React Basics/Week 1/Your First Component/src/App.js
@@ -1,10 +1,18 @@
+import "./App.css";
+import Card from "./Card";
 import Heading from "./Heading";
+
 
 function App() { 
   return ( 
     <div className="App"> 
-      <Heading /> 
-    </div> 
+      <Heading firstName = "Firas"/> 
+
+      <h1>Task: Add three Card elements</h1> 
+      <Card h2 = "First card's h2" h3 = "First card's h3"/>
+      <Card h2 = "Second card's h2" h3 = "Second card's h3"/>
+      <Card h2 = "Third card's h2" h3 = "Third card's h3"/>
+    </div>
   ); 
 } 
  
